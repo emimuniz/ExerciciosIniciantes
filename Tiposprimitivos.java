8c4b1137eb3f114e8abc22684c01c7d251d9185e
Emillyn Muniz <emillyn_muniz@hotmail.com>
2017-10-02 22:23:11 -0300

    Terceiros commit

---
+++ Tiposprimitivos.java
@@ -0,0 +1,17 @@
+
+package tiposprimitivos;
+
+import java.util.Scanner;
+
+public class Tiposprimitivos {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o nome do aluno: ");
+        String nome = teclado.nextLine();
+        System.out.println("Digite a nota do aluno: ");
+        float nota = teclado.nextFloat();
+        System.out.printf("A nota de "+ nome + " é %.2f \n",nota);
+    }
+    
+}
